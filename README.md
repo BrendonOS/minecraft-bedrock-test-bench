004f5424e5b0a35d605501c9b5aea4d980ad5ded
mhsjlw <mhsjlw@users.noreply.github.com>
2016-08-11 23:28:49 -0400

    markdown

---
+++ README.md
@@ -0,0 +1,11 @@
+spiffy
+======
+
+A Minecraft: PE server in Python
+
+## Installation
+It's easy, make sure you've got Python 3.5 installed, then,
+```sh
+$ pip install -r requirements.txt
+$ python main.py
+```
--- proto.py
+++ proto.py
@@ -1,18 +1,3 @@
 packetIds = {
-    "Identification": 0x00,
-    "Ping": 0x01,
-    "LevelInitialize": 0x02,
-    "LevelDataChunk": 0x03,
-    "LevelFinalize": 0x04,
-    "ClientSetBlock": 0x05,
-    "SetBlock": 0x06,
-    "SpawnPlayer": 0x07,
-    "PositionAndOrientation": 0x08,
-    "PositionAndOrientationUpdate": 0x09,
-    "PositionUpdate": 0x0a,
-    "OrientationUpdate": 0x0b,
-    "DespawnPlayer": 0x0c,
-    "Message": 0x0d,
-    "DisconnectPlayer": 0x0e,
-    "UpdateUserType": 0x0f
+    "game_login": 0x00,
 }
